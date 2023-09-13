8e3e72c8fc3d9a104a5379c34cd383dd69a3d6cb
nagimbsher <49290690+nagimb@users.noreply.github.com>
2023-09-13 22:03:25 +0300

    first commit

---
+++ src/Top.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Top() {
+  return (
+    <header>
+      <h1>React Typing Game</h1>
+      <h3>with Hooks and Ref</h3>
+      <h5>
+        by{' '}
+        <a className="secondary-color" href="https://github.com/nagimbsher/nagimbsher">
+          @nagimbsher
+        </a>
+      </h5>
+    </header>
+  )
+}
+
+export default Top
